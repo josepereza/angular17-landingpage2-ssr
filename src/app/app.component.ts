868c8f5a9f5344778d81e4eab98101fc6102391d
josepereza <josepereza66@gmail.com>
2024-01-27 19:00:24 +0100

    first commit2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ProductoComponent } from './components/producto/producto.component';
+import { FeaturetteComponent } from './components/featurette/featurette.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,NavbarComponent, HeaderComponent, ProductoComponent,FeaturetteComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
